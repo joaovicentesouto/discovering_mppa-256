484a122800e026ce00be204b068d4c0ea753cfed
João Vicente Souto <joaovicentesouto@gmail.com>
2018-10-08 20:22:42 -0300

    Change make for compile two syncs folders

--- script/mppa.sh
+++ script/mppa.sh
@@ -9,7 +9,7 @@ export K1DIR=/usr/local/k1tools/bin
 # Default Parameters.
 export NPROCS=2
 
-for kernel in sync_exec;
+for kernel in sync_1_exec sync_2_exec;
 do
 	echo "  ========== Running SYNC Kernel ==========  "
 	$K1DIR/k1-jtag-runner                               \
