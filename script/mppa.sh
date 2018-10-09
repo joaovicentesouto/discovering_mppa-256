c1ae134183f3f5111ca80d780badaae0681e0bf7
João Vicente Souto <joaovicentesouto@gmail.com>
2018-10-09 16:40:31 -0300

    Update script

--- script/mppa.sh
+++ script/mppa.sh
@@ -9,10 +9,32 @@ export K1DIR=/usr/local/k1tools/bin
 # Default Parameters.
 export NPROCS=2
 
-for kernel in sync_1_exec;
-do
-	echo "  ========== Running SYNC Kernel ==========  "
-	$K1DIR/k1-jtag-runner                               \
-		--multibinary=$BINDIR/$kernel.img               \
-		--exec-multibin=IODDR0:io_bin
-done
+if [ -z $1 ]
+then
+	echo "You must specify ipc or noc mode."
+	exit 1
+fi
+
+if [ "$1" = "ipc" ];
+then
+	echo "  ========== IPC MODE ==========  "
+	for kernel in sync_exec;
+	do
+		echo "  ========== Running SYNC Kernel ==========  "
+		$K1DIR/k1-jtag-runner                               \
+			--multibinary=$BINDIR/$kernel.img               \
+			--exec-multibin=IODDR0:master
+	done
+elif [ "$1" = "noc" ];
+then
+	echo "  ========== NOC MODE ==========  "
+	for kernel in sync_exec;
+	do
+		echo "  ========== Running SYNC Kernel ==========  "
+		$K1DIR/k1-jtag-runner                               \
+			--multibinary=$BINDIR/$kernel.img               \
+			--exec-multibin=IODDR0:io_bin
+	done
+fi
+
+
